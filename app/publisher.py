e7aa5490ed1c352d55832061da83a471f6b17ace
Harry Kodden <harry@e-tunity.nl>
2022-08-25 09:42:21 +0200

    wip

--- app/publisher.py
+++ app/publisher.py
@@ -74,43 +74,28 @@ my_control = Control(
   password = os.environ.get('API_PASS', "guest")
 )
 
-def enable_services(services):
+def enable_service(service_name, service_password):
 
-  for service in services:
+  logger.info(f"Enabling service: '{service_name}'...")
+  my_control.api(f"/api/vhosts/{service_name}", method='PUT')
 
-    try:
-      service_name, service_password = [x.strip() for x in service.split('=')]
+  logger.info(f"Adding credentials for service: '{service_name}'...")
+  my_control.api(f"/api/users/{service_name}", method='PUT', payload={ 
+      "password": service_password, 
+      "tags": ""
+    }
+  )
 
-      if (service_name == ""):
-        raise Exception("Service name can not be blank !")
+  logger.info(f"Adding permissions for service: '{service_name}'...")
+  my_control.api(f"/api/permissions/{service_name}/{service_name}", method='PUT', payload={ 
+      "configure": ".*", 
+      "write": ".*",
+      "read": ".*"
+    }
+  )
 
-      if (service_password == ""):
-        raise Exception("Service password can not be blank !")
-
-    except Exception as e:
-      logger.error(f"Configuration error: {str(e)}")
-      continue
-
-    logger.info(f"Enabling service: '{service_name}'...")
-    my_control.api(f"/api/vhosts/{service_name}", method='PUT')
-
-    logger.info(f"Adding credentials for service: '{service_name}'...")
-    my_control.api(f"/api/users/{service_name}", method='PUT', payload={ 
-        "password": service_password, 
-        "tags": ""
-      }
-    )
-
-    logger.info(f"Adding permissions for service: '{service_name}'...")
-    my_control.api(f"/api/permissions/{service_name}/{service_name}", method='PUT', payload={ 
-        "configure": ".*", 
-        "write": ".*",
-        "read": ".*"
-      }
-    )
-    logger.info(f"Service: '{service_name}' is now enabled !")
+  logger.info(f"Service: '{service_name}' is now enabled !")
     
-
 def notify_service(service, data):
   for k,v in data.items():
     logger.info(f"Notify {service_name} for update on '{k}' value: '{v}'...")
@@ -130,7 +115,22 @@ if __name__ == "__main__":
     logger.error(f"No Services configured !")
     sys.exit(-1)
 
-  enable_services(services)
+  for service in services:
+  
+    try:
+      service_name, service_password = [x.strip() for x in service.split('=')]
+
+      if (service_name == ""):
+        raise Exception("Service name can not be blank !")
+
+      if (service_password == ""):
+        raise Exception("Service password can not be blank !")
+
+      enable_service(service_name, service_password)
+
+    except Exception as e:
+      logger.error(f"Configuration error: {str(e)}")
+      continue
 
   topics = ['user', 'group']
 
